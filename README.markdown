b33c691e159c0334aebe25398bb4deafdad8be73
Hishal <hishalv@gmail.com>
2011-09-14 10:33:06 +0200

    revised uploads

--- README.markdown
+++ README.markdown
@@ -3,4 +3,12 @@
 The is a sample app using carrierwave and carrierwave_direct gems to upload directly to amazon s3.
 This is my first commit to github.
 
+Steps to get working:
+
+1. add credentials to config/initializers/carrierwave.rb
+2. create database in database.yml
+3. migrate database
+
+also update gems
+
 Thanks
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => 'AKIAJ2FUOM4X7CAHTKDQ',       # required
-    :aws_secret_access_key  => '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',       # required
+    :aws_access_key_id      => 'my_acccess_id',       # required
+    :aws_secret_access_key  => 'my_secret_access_key',       # required
     :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'spatialscapebucket-00300'                     # required
-  config.fog_host       = 'https://spatialscapebucket-00300.s3.amazonaws.com'            # optional, defaults to nil
+  config.fog_directory  = 'my_bucket'                     # required
+  config.fog_host       = 'https://#{fog_directory}.s3.amazonaws.com'            # optional, defaults to nil
   config.fog_public     = false                                   # optional, defaults to true
 end
